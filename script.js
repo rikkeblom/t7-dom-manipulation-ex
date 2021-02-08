46cfaf29080e773284e6816dfdf28e1c69a45e8e
Rikke Blom <rikk957v@stud.kea.dk>
2021-02-08 11:57:32 +0100

    Finished

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //Write your own code here
+
+const sections = document.querySelectorAll("section");
+sections.forEach(color);
+
+function color(sec) {
+  sec.classList.add("filled");
+  sec.previousElementSibling.remove();
+}
+
+const bre = document.querySelectorAll("br");
+bre.forEach(remBR);
+
+function remBR(e) {
+  e.remove();
+}
+
 //DON'T touch any other files!
